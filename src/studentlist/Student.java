6e23c86f53cc13ccd0f86aef46f0bbe28559675f
nguye617 <nguye617@sheridancollege.ca>
2024-10-04 11:58:51 -0400

    Exercise Commit2

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -2,21 +2,46 @@ package studentlist;
 
 /**
  *
- * @author paulbonenfant
+ * @author DongNguyen
  */
 public class Student {
 
-    private String name;
+   private String name;
+   private String studentID;
+   private String address;
 
-    public Student(String name) {
-        this.name = name;
-    }
-    
+   // Constructor to initialize the student with a name
+   public Student(String givenName) {  // Make the constructor public
+       this.name = givenName;
+   }
+
+   // Getter for 'name'
     public String getName() {
         return name;
     }
 
+    // Setter for 'name'
     public void setName(String name) {
         this.name = name;
-    } 
+    }
+
+    // Getter for 'studentID'
+    public String getStudentID() {
+        return studentID;
+    }
+
+    // Setter for 'studentID'
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    // Getter for 'address'
+    public String getAddress() {
+        return address;
+    }
+
+    // Setter for 'address'
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
